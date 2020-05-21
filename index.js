e8c687d679e6dc4516142e76f972ed1c5bf79c33
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-05-21 13:33:35 +0530

    code running successfully with taskList. Now have to do without taskList

--- index.js
+++ index.js
@@ -87,3 +87,25 @@ console.log(scheduler(["A","B","C","D"],1)===4);
 console.log(scheduler(["A","A","A"],0)===3);
 console.log(scheduler(["A","A","A"],5)===13);
 console.log(scheduler(["A","A","B","B","A","A"],4)===16);
+
+
+// Given a char array representing tasks CPU need to do. It contains capital letters A to Z where different letters represent different tasks. Tasks could be done without original order. Each task could be done in one interval. For each interval, CPU could finish one task or just be idle.
+
+// However, there is a non-negative cooling interval n that means between two same tasks, there must be at least n intervals that CPU are doing different tasks or just be idle.
+
+// You need to return the least number of intervals the CPU will take to finish all the given tasks.
+
+// Examples
+// Input: tasks = ["A","A","A","B","B","B"], n = 2
+// Output: 8
+// Explanation: A -> B -> idle -> A -> B -> idle -> A -> B.
+
+// Input: tasks = ["A","A","A","B","B","B"], n = 3
+// Output: 10
+// Explanation: A -> B -> idle -> idle -> A -> B -> idle -> idle -> A -> B.
+
+// Input: ["A","A","A","B","B","B","C","D","E","F","G","H","I","J","K"], n = 7
+// Output: 18
+// Constraints:
+// The number of tasks is in the range [1, 10000].
+// The integer n is in the range [0, 100].
